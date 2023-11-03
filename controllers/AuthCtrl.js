cf6805f2cca9616f0e2da494a0cf05c752280696
Aditya Rana <adityarajputrana2016@gmail.com>
2023-11-04 00:19:39 +0530

    Change

--- controllers/AuthCtrl.js
+++ controllers/AuthCtrl.js
@@ -2,7 +2,7 @@ import sendReponse from "./ResponseCtrl.js";
 export const DoctorLogin = (req, res) => {
     const responseObject = {
             jwt: "your_jwt_token_here",
-            expires_at: 1678118400000, // Replace with the actual timestamp in milliseconds
+            expiresAt: 1678118400000, // Replace with the actual timestamp in milliseconds
             uid: "user_id_here",
             user: {
                 id: "user_id_here",
