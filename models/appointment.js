56c639ad91cae5201060458785bac263d24e5cc3
vikas <vikash.agarawal2012@gmail.com>
2024-10-01 00:05:30 +0530

    time data model updatation

--- models/appointment.js
+++ models/appointment.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const appointmentSchema = new mongoose.Schema({
     patient_id: {type: mongoose.Schema.Types.ObjectId, ref: 'HospitalsPatient',required: [true,"Please Enter Patient Id"]},
     appt_date: { type: Date,required: [true, "Please Enter Appointment Date"],},
-    appt_time: { type: Number,required: [true, "Please Enter Category Name"],},
+    appt_time: { type: String,required: [true, "Please Enter Category Name"],},
     creator_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Staff',required: [true, "Please Enter Creator Id"]},
     createdAt: { type: Date, default: Date.now },
 });
